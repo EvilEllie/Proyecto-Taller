2d09ff89c9d18c0a00cfe5f9858b6e4064aeead0
Sandy Torres <sandra.torres.esc@gmail.com>
2026-04-14 06:58:47 -0600

    Conectar base de datos a Railway

--- config.py
+++ config.py
@@ -3,7 +3,8 @@
 class Config:
     SECRET_KEY = 'inventario_taller_secretkey_2026'
     
-    MYSQL_HOST = 'localhost'
+    MYSQL_HOST = 'mysql.railway.internal'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''          # Tu contraseña aquí
-    MYSQL_DB = 'inventario_taller'
+    MYSQL_PASSWORD = 'BmjCFHlOpjLPjVXMroXskaEZBrKNIJai'
+    MYSQL_DB = 'railway'
+    MYSQL_PORT = 3306
